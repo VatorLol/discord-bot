9e880554fcbaef8da8bd66c5f8f8a4c65fddb8c1
vatorlol <eyub2001@gmail.com>
2018-10-20 16:58:26 +0300

    removed RiotAPITokenProvider class added Orianna framework for riot api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,16 @@
+import com.merakianalytics.orianna.Orianna;
 import sx.blah.discord.api.IDiscordClient;
 
 public class Main {
 
-    // Gets a provider instance
-    public static RiotAPITokenProvider provider = RiotAPITokenProvider.getInstance();
-
     public static void main(String[] args) {
+        // Creating new configuration file for Orianna framework to setup Riot API Key
+        Orianna.Configuration config = new Orianna.Configuration();
+
         // Token to use creating IDiscordClient
         var token = args[0];
         // IDiscordClient that uses getDiscordClient helper method in BotUtils to create client
         IDiscordClient client = BotUtils.getDiscordClient(token);
-
-        // Uses provider instance to set token of riot api s
-        provider.setTOKEN(args[1]);
 
         // Registering all @EventSubscribers at BotEvents class
         client.getDispatcher().registerListeners(new BotEvents());
